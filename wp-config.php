e88637f52aecc2650b1040535280800a1ac2651e
elgammalqa <elgammalqa@gmail.com>
2021-02-11 12:34:15 +0200

    add db2

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dpfm6vlbsh38u' );
+define( 'DB_NAME', 'postgresql-rigid-14098' );
 
 /** MySQL database username */
 define( 'DB_USER', 'plorqqeyghdtyg' );
